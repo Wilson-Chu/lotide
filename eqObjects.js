918ce1f21cc0a982a7599ab9335ac990e5e9bf8d
Wilson Chu <wilson1.chu@gmail.com>
2023-09-21 08:03:20 -0400

    completed replimin. step 3 of LHL activity

--- eqObjects.js
+++ eqObjects.js
@@ -9,7 +9,34 @@ const assertEqual = function (actual, expected) {
 
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
+
+// if (arr1.length !== arr2.length) {
+//   return false;
+// }
+
+// for (let i = 0; i < arr1.length; i++) {
+//   if (arr1[i] !== arr2[i]) {
+//     return false;
+//   }
+// }
+
+// return true;
+// };
 const eqObjects = function (object1, object2) {
+  let objKeys1 = Object.keys(object1); // Array of keys
+  let objKeys2 = Object.keys(object2);
+
+  if (objKeys1.length !== objKeys2.length) {
+    return false;
+  }
+
+  for (let objKey1 of objKeys1) {
+    if (objKeys1[objKey1] !== objKeys2[objKey1]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
 // TEST CODE
