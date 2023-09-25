b8991429904b11c7c55aa6dab4046fdfc40df457
Wilson Chu <wilson1.chu@gmail.com>
2023-09-25 19:40:29 -0400

    refactored some test cases using mocha & chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,12 +1,24 @@
-const assertEqual = require('../assertEqual');
-const head = require('../head.js');
+// const assertEqual = require('../assertEqual');
+// const head = require('../head.js');
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(8, 8.00);
-assertEqual('pizza', 'pizza');
-assertEqual(42, 24);
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), head([0]));
+// // TEST CODE
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
+// assertEqual(8, 8.00);
+// assertEqual('pizza', 'pizza');
+// assertEqual(42, 24);
+// assertEqual(head([5, 6, 7]), 5);
+// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+// assertEqual(head([]), head([0]));
+
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+});
