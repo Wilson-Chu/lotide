e47425069cb2d5af35cc2e10b9046732a4d72aee
Wilson Chu <wilson1.chu@gmail.com>
2023-09-25 22:24:40 -0400

    refactored for test cases

--- test/headTest.js
+++ test/headTest.js
@@ -1,24 +1,17 @@
-// const assertEqual = require('../assertEqual');
-// const head = require('../head.js');
-
-// // TEST CODE
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual(8, 8.00);
-// assertEqual('pizza', 'pizza');
-// assertEqual(42, 24);
-// assertEqual(head([5, 6, 7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(head([]), head([0]));
-
 const assert = require('chai').assert;
-const head   = require('../head');
+const head = require('../head');
 
 describe("#head", () => {
   it("returns 1 for [1, 2, 3]", () => {
     assert.strictEqual(head([1, 2, 3]), 1);
   });
   it("returns '5' for ['5']", () => {
-    assert.strictEqual(head(['5']), '5'); 
+    assert.strictEqual(head(['5']), '5');
+  });
+  it("returns 'Bootcamp' for ['Bootcamp', 'Is', 'Cool']", () => {
+    assert.strictEqual(head(['Bootcamp', 'Is', 'Cool']), 'Bootcamp');
+  });
+  it("returns 'undefined' for []", () => {
+    assert.strictEqual(head([]), undefined);
   });
 });
